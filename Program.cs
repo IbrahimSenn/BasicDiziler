2bf94da05e0f284301e9a391e54b7e2119faa202
Monsterter <ibrahimsenc@gmail.com>
2023-10-18 10:44:28 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Security.Principal;
+
+Console.WriteLine("Lütfen dizinin eleman sayısını giriniz..");
+
+int diziLength = int.Parse(Console.ReadLine());
+
+int[] sayilar = new int[diziLength];
+for (var i = 0; i < diziLength; i++)
+{
+    Console.WriteLine("Lütfen dizinin {0}. elemanlarını giriniz..",i+1);
+    sayilar[i] = int.Parse(Console.ReadLine());
+
+}
+int toplam =0;
+foreach (var sayi in sayilar)
+{
+    toplam += sayi;
+}
+Console.WriteLine("Dizinizin ortalamsı :" + (toplam/diziLength) );
---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+    Kulanıcının girdiği sayılar ile dizi oluşturup, bu dizideki elemanlerın ortalamssını alan program.
